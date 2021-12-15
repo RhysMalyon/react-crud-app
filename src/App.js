8d77b60e2a702755c079b3ee2c4b31cdce165ec3
Rhys Malyon <malyonr@gmail.com>
2021-12-15 17:19:17 +0900

    Make api calls for posts and users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import api from './api/api';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await api.get('/posts')
+      setPosts(response.data)
+    }
+
+    const getUsers = async () => {
+      const response = await api.get('/users')
+      setUsers(response.data)
+    }
+
+    getPosts()
+    getUsers()
+  }, [])
+
+  const findUser = (id) => {
+    const foundUser = users.filter(user => user.id === id)
+    return foundUser[0].name
+  }
+
   return (
     <div className="App">
-      Content
+      {posts.map(post => {
+        return (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{findUser(post.userId)}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
